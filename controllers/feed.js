a198d54c693dbeac14c11385c3d70c3a94980e21
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 12:32:38 -0300

    feat(feed): pagination and totalItems by page

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,11 +5,22 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const { post } = require("../routes/feed");
 exports.getPosts = (req, res, next) => {
+	const currentPage = req.query.page || 1;
+	const perPage = 2;
+	let totalItems;
 	Post.find()
+		.countDocuments()
+		.then((count) => {
+			totalItems = count;
+			return Post.find()
+				.skip((currentPage - 1) * perPage)
+				.limit(perPage);
+		})
 		.then((posts) => {
 			res.status(200).json({
 				message: "Fetched posts successfully",
 				posts,
+				totalItems,
 			});
 		})
 		.catch((err) => {
