f9be75b8a7a9aa955b2608ad27b49f57678ef141
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 13:28:40 -0300

    feat: signup users and validation req body

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,28 @@
 const express = require("express");
-
+const { body } = require("express-validator");
 const router = express.Router();
-
-router.put("/signup");
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+router.put(
+	"/signup",
+	[
+		body("email")
+			.isEmail()
+			.withMessage("PLease enter a valid email")
+			.custom((value, { req }) => {
+				return User.findOne({ email: value }).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject(
+							"E-mail address already exists!."
+						);
+					}
+				});
+			})
+			.normalizeEmail(),
+		body("password").trim().isLength({ min: 5 }),
+		body("name").trim().not().isEmpty(),
+	],
+	authController.signup
+);
 
 module.exports = router;
