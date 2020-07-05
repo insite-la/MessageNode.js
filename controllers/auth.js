14031da8b32ee7c1abd8c02384588864fadde42e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 16:15:46 -0300

    feat: loggin in with json web token authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 exports.signup = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
@@ -27,6 +28,56 @@ exports.signup = (req, res, next) => {
 				message: "User created!",
 				userId: result._id,
 			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+exports.login = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	let loadedUser;
+	User.findOne({ email })
+		.then((user) => {
+			if (!user) {
+				const error = new Error(
+					"A user with this email could not be found"
+				);
+				error.statusCode = 401;
+				throw error;
+			}
+			loadedUser = user;
+			bcrypt
+				.compare(password, user.password)
+				.then((isEqual) => {
+					if (!isEqual) {
+						const error = new Error("Wrong password");
+						error.statusCode = 401;
+						throw error;
+					}
+					const token = jwt.sign(
+						{
+							email: loadedUser.email,
+							userId: loadedUser._id.toString(),
+						},
+						"secret",
+						{ expiresIn: "1h" }
+					);
+					res.status(200).json({
+						token,
+						userId: loadedUser._id.toString(),
+					});
+				})
+				.catch((err) => {
+					if (!err.statusCode) {
+						err.statusCode = 500;
+					}
+					next(err);
+				});
 		})
 		.catch((err) => {
 			if (!err.statusCode) {
