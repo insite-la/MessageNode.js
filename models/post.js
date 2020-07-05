90a336cea40c39f4fbe8b98e04af1bd50474650f
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 19:05:44 -0300

    chore(feed): connected post with users

--- models/post.js
+++ models/post.js
@@ -16,8 +16,9 @@ const postSchema = new Schema(
 			require: true,
 		},
 		creator: {
-			type: Object,
-			require: String,
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
 		},
 	},
 	{ timestamps: true }
