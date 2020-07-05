9ae5bf7b292f88529b26cfd0313c10c0d5938219
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 19:18:31 -0300

    chore: added authorization checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -124,6 +124,11 @@ exports.editPost = (req, res, next) => {
 				error.statusCode = 404;
 				throw error;
 			}
+			if (post.creator.toString() !== req.userId) {
+				const error = new Error("Not authorizated!");
+				error.statusCode = 403;
+				throw error;
+			}
 			if (imageUrl !== post.imageUrl) {
 				clearImage(post.imageUrl);
 			}
@@ -150,6 +155,11 @@ exports.deletePost = (req, res, next) => {
 			if (!post) {
 				const err = new Error("Could not find post.");
 				error.statusCode = 404;
+				throw error;
+			}
+			if (post.creator.toString() !== req.userId) {
+				const error = new Error("Not authorizated!");
+				error.statusCode = 403;
 				throw error;
 			}
 			clearImage(post.imageUrl);
