69aa9b7830c008013a48db37bc057111d85155d1
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-06 00:40:35 -0300

    test: should throw an error if no authorization header is present

---
+++ test/auth-middleware.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect;
+const authMiddleware = require("../middleware/is-auth");
+it("should throw an error if no authorization header is present", () => {
+	const req = {
+		get: (headerName) => null,
+	};
+	expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+		"Not authenticated"
+	);
+});
