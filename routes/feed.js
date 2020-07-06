687f84a55d070dd3cd976ec4b3ea008ceb2f439d
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-05 22:19:48 -0300

    refactor: change then-catch block for async/await keywords

--- routes/feed.js
+++ routes/feed.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { body } = require("express-validator");
+
 const feedController = require("../controllers/feed");
 const isAuth = require("../middleware/is-auth");
+
 const router = express.Router();
 
 // GET /feed/posts
@@ -27,7 +29,9 @@ router.put(
 		body("title").trim().isLength({ min: 5 }),
 		body("content").trim().isLength({ min: 5 }),
 	],
-	feedController.editPost
+	feedController.updatePost
 );
+
 router.delete("/post/:postId", isAuth, feedController.deletePost);
+
 module.exports = router;
